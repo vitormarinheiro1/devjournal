b54cdc9118ecfc6cc3ab8a75d32c8f5d393d6684
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-26 08:48:15 -0300

    fix: login with google

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,13 +1,16 @@
 'use client';
 import Link from 'next/link';
 import { Card, CardContent } from './ui/card';
-import { Code2, MenuIcon } from 'lucide-react';
+import { Code2, Loader, LogOut, MenuIcon } from 'lucide-react';
 import { Button } from './ui/button';
 import Image from 'next/image';
 import GoogleICO from '../../public/google.svg';
-import { signIn, signOut } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { Avatar, AvatarImage } from './ui/avatar';
 
 const Header = () => {
+  const { status, data } = useSession();
+
   async function handleLogin() {
     await signIn();
   }
@@ -29,12 +32,30 @@ const Header = () => {
 
         {/* TODO: Use useSession for authenticated and unauthenticated users. */}
 
-        <div>
+        {status === 'loading' && (
+          <button className="animate-spin">
+            <Loader size={26} color="#4b5563" />
+          </button>
+        )}
+
+        {status === 'unauthenticated' && (
           <Button className="font-semibold" onClick={handleLogin}>
             <Image src={GoogleICO} alt="Google Icon" width={20} height={20} className="mr-1" />
             Continue with Google
           </Button>
-        </div>
+        )}
+
+        {status === 'authenticated' && (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ''} />
+            </Avatar>
+            <p className="font-medium">{data.user?.name}</p>
+            <button onClick={handleLogout}>
+              <LogOut size={26} color="#ff0000" />
+            </button>
+          </div>
+        )}
 
         {/* MOBILE
         <div className="flex flex-col sm:hidden sm:gap-4 sm:py-4 sm:pl-14">
