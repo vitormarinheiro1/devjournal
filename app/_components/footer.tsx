1c85833c2fec69430b1a5765ebe4402d9cf7c79e
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-26 15:47:57 -0300

    style: footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="flex flex-row items-center gap-2">
         <div className="py-2 text-xs text-zinc-300">© 2024 DevTrek. All rights reserved.</div>
         <button>
-          <Link href="https://github.com/vitormarinheiro1/devtrek">
+          <Link href="https://github.com/vitormarinheiro1/devtrek" target="_blank">
             <FaGithub className="hover:text-gray-400" size={18} />
           </Link>
         </button>
